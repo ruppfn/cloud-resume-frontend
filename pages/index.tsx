8fc1fb4e3b6cda1453516b2465999ea48f26f0d9
Franco Nahuel Rupp <ruppfn@gmail.com>
2022-09-25 18:40:28 -0300

    move components to components folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import type {NextPage} from 'next'
 import styles from '../styles/Home.module.css'
 import {CvData} from "../types/cvData";
-import ContactInfoComponent from "./_contactInfo";
-import ExperienceComponent from "./_experience";
-import SkillsComponent from "./_skills";
+import ContactInfoComponent from "../components/_contactInfo";
+import ExperienceComponent from "../components/_experience";
+import SkillsComponent from "../components/_skills";
 
 interface Props {
     cvData: CvData
